d3050671fc701108ee0a92c5e544db5ef01532c0
Josh Siva <jtsiva@gmail.com>
2018-06-05 23:46:24 -0400

    BleReader mostly implemented. WIP. Need to rework MessageRetriever in bluenet_stack

--- app/src/main/java/com/example/marco/bluenet_01/BLE_Comm/BleReader.java
+++ app/src/main/java/com/example/marco/bluenet_01/BLE_Comm/BleReader.java
@@ -6,10 +6,9 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
-import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
@@ -24,17 +23,17 @@ import android.util.Log;
 
 import com.example.marco.bluenet_01.BuildConfig;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.UUID;
 
 import nd.edu.bluenet_stack.AdvertisementPayload;
 import nd.edu.bluenet_stack.LayerBase;
 import nd.edu.bluenet_stack.Query;
 import nd.edu.bluenet_stack.Reader;
+import nd.edu.bluenet_stack.MessageRetriever;
 
 import static android.bluetooth.BluetoothGatt.GATT_SUCCESS;
 
@@ -45,15 +44,37 @@ import static android.bluetooth.BluetoothGatt.GATT_SUCCESS;
 
 public class BleReader extends LayerBase
         implements
-        Reader,
         Query{
 
     private static final String ERR_TAG = "FATAL ERROR";
     private static final String INFO_TAG = "APP_INFO";
     private static final String DEBUG_TAG = "DEBUG";
 
-    public static final ParcelUuid BASIC_AD =   //used as an agreement for ad/sc
-            ParcelUuid.fromString("00001860-0000-1000-8000-00805f9b34fb");
+    //service UUID
+    public static final UUID BLUENET_SERVICE_UUID =
+            UUID.fromString("00001860-0000-1000-8000-00805f9b34fb");
+
+
+    //UUIDs for different messsage types
+    private static final UUID SMALL_MSG_CHAR_UUID =
+            UUID.fromString("0000" + AdvertisementPayload.SMALL_MESSAGE_STR + "-0000-1000-8000-00805f9b34fb");
+    private static final UUID REGULAR_MSG_CHAR_UUID =
+            UUID.fromString("0000" + AdvertisementPayload.REGULAR_MESSAGE_STR + "-0000-1000-8000-00805f9b34fb");
+    private static final UUID LOCATION_UPDATE_CHAR_UUID =
+            UUID.fromString("0000" + AdvertisementPayload.LOCATION_UPDATE_STR + "-0000-1000-8000-00805f9b34fb");
+    private static final UUID GROUP_QUERY_CHAR_UUID =
+            UUID.fromString("0000" + AdvertisementPayload.GROUP_QUERY_STR + "-0000-1000-8000-00805f9b34fb");
+    private static final UUID GROUP_UPDATE_CHAR_UUID =
+            UUID.fromString("0000" + AdvertisementPayload.GROUP_UPDATE_STR + "-0000-1000-8000-00805f9b34fb");
+
+    //UUID for messages that are pulled
+    private static final UUID PULL_MESSAGE_CHAR_UUID =
+            UUID.fromString("31517c58-66bf-470c-b662-e352a6c80cba");
+
+
+    //for configuring notifications
+    private static final UUID CLIENT_CHAR_CONFI_UUID = UUID.
+            fromString("00002a08-0000-1000-8000-00805f9b34fb");
 
     public Context context;
     public Activity activity;
@@ -61,21 +82,67 @@ public class BleReader extends LayerBase
 
     private static final int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothManager mBluetoothManager;
     private BluetoothLeScanner mBluetoothLeScanner;
-    public BluetoothGatt mBluetoothGatt;
-//    private BluetoothGattCallback mGattCallback;
 
     private int mConnectionState = STATE_DISCONNECTED;
     private static final int STATE_DISCONNECTED = 0;
     private static final int STATE_CONNECTING = 1;
     private static final int STATE_CONNECTED = 2;
 
-    public List<BluetoothGattService> mBluetoothGattService;
-//    public HashMap<BluetoothDevice, byte[]> mBleDevicesDict;
-    public Set<BluetoothDevice> mBleDeviceSet;
+   // public List<BluetoothGattService> mBluetoothGattService;
+    private Map<String, byte[]> mBTToBNAddrMap = new HashMap<String, byte[]>();
+   // public Set<BluetoothDevice> mBleDeviceSet;
+    private Map<String, BluetoothGatt> mConnectedDeviceMap = new HashMap<String, BluetoothGatt>();
+    private Map<String, Integer> mServiceSetupTables = new HashMap<String, Integer>();
+    private List<UUID> mCharactericUUIDList = new ArrayList<UUID>();
+
+    private UUID getUUID(int msgType) {
+        switch (msgType) {
+            case AdvertisementPayload.SMALL_MESSAGE:
+                return SMALL_MSG_CHAR_UUID;
+            case AdvertisementPayload.REGULAR_MESSAGE:
+                return REGULAR_MSG_CHAR_UUID;
+            case AdvertisementPayload.LOCATION_UPDATE:
+                return LOCATION_UPDATE_CHAR_UUID;
+            case AdvertisementPayload.GROUP_QUERY:
+                return GROUP_QUERY_CHAR_UUID;
+            case AdvertisementPayload.GROUP_UPDATE:
+                return GROUP_UPDATE_CHAR_UUID;
+            default:
+                return null;
+        }
+    }
+
+    private int getMsgType(UUID uuid) {
+        int msgType = 0;
+        if (SMALL_MSG_CHAR_UUID.equals(uuid) ) {
+            msgType = AdvertisementPayload.SMALL_MESSAGE;
+        }
+        else if (REGULAR_MSG_CHAR_UUID.equals(uuid)) {
+            msgType = AdvertisementPayload.REGULAR_MESSAGE;
+        }
+        else if (LOCATION_UPDATE_CHAR_UUID.equals(uuid)) {
+            msgType = AdvertisementPayload.LOCATION_UPDATE;
+        }
+        else if (GROUP_QUERY_CHAR_UUID.equals(uuid)) {
+            msgType = AdvertisementPayload.GROUP_QUERY;
+        }
+        else if (GROUP_UPDATE_CHAR_UUID.equals(uuid)) {
+            msgType = AdvertisementPayload.GROUP_UPDATE;
+        }
+
+        return msgType;
+    }
 
 
     public BleReader(Context context, Activity activity){
+        mCharactericUUIDList.add(SMALL_MSG_CHAR_UUID);
+        mCharactericUUIDList.add(REGULAR_MSG_CHAR_UUID);
+        mCharactericUUIDList.add(LOCATION_UPDATE_CHAR_UUID);
+        mCharactericUUIDList.add(GROUP_QUERY_CHAR_UUID);
+        mCharactericUUIDList.add(GROUP_UPDATE_CHAR_UUID);
+
         this.context = context.getApplicationContext();
         this.activity = activity.getParent();
         this.originalName = activity.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE).getString("originalName", "");
@@ -87,8 +154,7 @@ public class BleReader extends LayerBase
         }
         // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
         // BluetoothAdapter through BluetoothManager.
-        final BluetoothManager mBluetoothManager =
-                (BluetoothManager) activity.getSystemService(context.BLUETOOTH_SERVICE);
+        mBluetoothManager = (BluetoothManager) activity.getSystemService(context.BLUETOOTH_SERVICE);
         mBluetoothAdapter = mBluetoothManager.getAdapter();
         // Checks if Bluetooth is supported on the device.
         if (mBluetoothAdapter == null) {
@@ -105,9 +171,6 @@ public class BleReader extends LayerBase
         }
 
         startLeScanning();
-//        mBleDevicesDict = new HashMap<BluetoothDevice, byte[]>();
-        mBleDeviceSet = new HashSet<BluetoothDevice>();
-
     }
 
     /**** **** BLE SCAN **** ****/
@@ -118,7 +181,7 @@ public class BleReader extends LayerBase
                 //.setDeviceName(string)
                 //.setManufacturerData()
                 //.setServiceData()
-                .setServiceUuid(BASIC_AD)
+                .setServiceUuid(new ParcelUuid(BLUENET_SERVICE_UUID))
                 .build();
 
         ArrayList<ScanFilter> filters = new ArrayList<ScanFilter>();
@@ -131,7 +194,7 @@ public class BleReader extends LayerBase
                 //.setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE) //AGGRESSIVE, STICKY  //require API 23
                 //.setNumOfMatches(ScanSettings.MATCH_NUM_MAX_ADVERTISEMENT) //ONE, FEW, MAX  //require API 23
                 //.setReportDelay(0) //0: no delay; >0: queue up
-                .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER) //LOW_POWER, BALANCED, LOW_LATENCY
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY) //LOW_POWER, BALANCED, LOW_LATENCY
                 .build();
 
         mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
@@ -170,60 +233,109 @@ public class BleReader extends LayerBase
         public void onScanFailed(int errorCode) {
             Log.e(ERR_TAG, "LE Scan Failed: " + errorCode);
         }
-
-        private void processResult(ScanResult result){
-            BluetoothDevice tempDevice = result.getDevice();
-            if(!mBleDeviceSet.contains(tempDevice)){
-                mBleDeviceSet.add(tempDevice);
-                Log.d(DEBUG_TAG,"Found a new device " + result.getDevice().getAddress());
-                connect(tempDevice);
-            }
-
-//            byte[] payload = result.getScanRecord().getServiceData().get(PAYLOAD_SERVICE);
-//            scan_Payload.fromBytes(payload);
-////            Log.d(INFO_TAG,"scan payload " + new String(payload,StandardCharsets.UTF_8));
-//            String userID = new String(scan_Payload.getSrcID(), StandardCharsets.UTF_8);
-//            if(!ID_Loc_Dict.containsKey(userID)){
-//                Location temp_loc = null;
-//                ID_Loc_Dict.put(userID,temp_loc);
-//                Log.d(INFO_TAG,"Found a new user nearby!" + " " + userID + result.getDevice().getAddress());
-//
-//            }
-//        }
-        }
     };
+
+    private void processResult(ScanResult result){
+        BluetoothDevice device = result.getDevice();
+        String deviceAddr = device.getAddress();
+
+        BluetoothDevice remoteDevice = mBluetoothAdapter.getRemoteDevice(deviceAddr);
+        int connectionState = mBluetoothManager.getConnectionState(remoteDevice, BluetoothProfile.GATT);
+
+        if(connectionState == BluetoothProfile.STATE_DISCONNECTED ){
+            // connect your device
+            device.connectGatt(this.context, false, mGattCallback);
+        }else if( connectionState == BluetoothProfile.STATE_CONNECTED ){
+            // already connected . send Broadcast if needed
+        }
+
+    }
+
+
     /**** **** end of BLE SCAN **** ****/
 
 
     /**** **** GATT Client **** ****/
+    private void setupCharacteristic(BluetoothGatt gatt) {
+        UUID currentUUID = mCharactericUUIDList.get(mServiceSetupTables.get(gatt.getDevice().getAddress()));
+        Integer index = mServiceSetupTables.get(gatt.getDevice().getAddress());
+
+        if (index < mCharactericUUIDList.size()) {
+            //Enable notifications locally for all msg type characteristics
+            BluetoothGattCharacteristic characteristic = gatt
+                    .getService(BLUENET_SERVICE_UUID)
+                    .getCharacteristic(currentUUID);
+
+            // Enable notifications for this characteristic locally
+            gatt.setCharacteristicNotification(characteristic, true);
+
+            // Write on the config descriptor to be notified when the value changes
+            BluetoothGattDescriptor descriptor =
+                    characteristic.getDescriptor(CLIENT_CHAR_CONFI_UUID);
+            descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+
+
+            mServiceSetupTables.put(gatt.getDevice().getAddress(), index + 1);
+
+            gatt.writeDescriptor(descriptor);
+        }
+    }
+
+
     private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            BluetoothDevice device = gatt.getDevice();
+            String address = device.getAddress();
+
             if (newState == BluetoothProfile.STATE_CONNECTED) {
-                mConnectionState = STATE_CONNECTED;
-//                broadcastUpdate("connected");
-                Log.d(DEBUG_TAG, "connected to " + gatt.getDevice().getAddress());
-                // Attempts to discover services after successful connection.
-                Log.d(DEBUG_TAG, "Attempting to start service discovery:" +
-                        mBluetoothGatt.discoverServices());
+                Log.i(INFO_TAG, "Connected to GATT server.");
+
+                if (!mConnectedDeviceMap.containsKey(address)) {
+                    mConnectedDeviceMap.put(address, gatt);
+                }
+                // Broadcast if needed
+                Log.i(INFO_TAG, "Attempting to start service discovery:" +
+                        gatt.discoverServices());
+
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                mConnectionState = STATE_DISCONNECTED;
-                Log.d(DEBUG_TAG, "disconnected: " + gatt.getDevice().getAddress());
+                Log.i(INFO_TAG, "Disconnected from GATT server.");
+                if (mConnectedDeviceMap.containsKey(address)){
+                    BluetoothGatt bluetoothGatt = mConnectedDeviceMap.get(address);
+                    if( bluetoothGatt != null ){
+                        bluetoothGatt.close();
+                        bluetoothGatt = null;
+                    }
+                    mConnectedDeviceMap.remove(address);
+                }
+                // Broadcast if needed
             }
+
+
         }
 
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             if (status == GATT_SUCCESS) {
-                //broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
-                Log.i(INFO_TAG,"GATT service discovered!");
-                mBluetoothGattService = gatt.getServices();
-//                if(gatt.getService(MSG_SERVICE_UUID) != null){
-//                    gatt.setCharacteristicNotification(gatt.getService(MSG_SERVICE_UUID).getCharacteristic(MSG_CHAR_UUID),true);
-//                }
-                Log.d(DEBUG_TAG,mBluetoothGattService.get(2).getUuid().toString());
+                BluetoothDevice device = gatt.getDevice();
+                String address = device.getAddress();
+                BluetoothGatt bluetoothGatt = mConnectedDeviceMap.get(address);
+                mServiceSetupTables.put(address, new Integer(0));
+
+                setupCharacteristic(bluetoothGatt);
             } else {
                 Log.e(ERR_TAG, "onServicesDiscovered received: " + status);
+            }
+        }
+
+        @Override
+        public void onDescriptorWrite(BluetoothGatt gatt,
+                                      BluetoothGattDescriptor descriptor, int status) {
+            BluetoothDevice device = gatt.getDevice();
+            String address = device.getAddress();
+            BluetoothGatt bluetoothGatt = mConnectedDeviceMap.get(address);
+            if (CLIENT_CHAR_CONFI_UUID.equals(descriptor.getUuid())) {
+               setupCharacteristic(bluetoothGatt);
             }
         }
 
@@ -232,66 +344,57 @@ public class BleReader extends LayerBase
                                          BluetoothGattCharacteristic characteristic,
                                          int status) {
             if (status == GATT_SUCCESS) {
-                //broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
+                BluetoothDevice device = gatt.getDevice();
+                String address = device.getAddress();
+                BluetoothGatt bluetoothGatt = mConnectedDeviceMap.get(address);
+
             }
         }
 
         @Override
         public void onCharacteristicChanged(BluetoothGatt gatt,
                                             BluetoothGattCharacteristic characteristic) {
-//            broadcastUpdate("char_changed", characteristic);
-            Log.d("CENTRAL","char changed");
-//                broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
-        }
+            BluetoothDevice device = gatt.getDevice();
+            String address = device.getAddress();
+            final BluetoothGatt bluetoothGatt = mConnectedDeviceMap.get(address);
 
-        @Override
-        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status){
-            if(status == GATT_SUCCESS){
-                Log.d("CENTRAL", "write to P");
+            AdvertisementPayload advPayload = new AdvertisementPayload();
+            byte[] data = characteristic.getValue();
+
+            if (advPayload.fromBytes(data)) {
+                //set msgType
+                advPayload.setMsgType(getMsgType(characteristic.getUuid()));
+
+                //Get BT-BN addr
+                if (advPayload.getTTL() == AdvertisementPayload.MAX_TTL) {
+                    mBTToBNAddrMap.put(address, advPayload.getSrcID());
+                }
+
+                //Get 1-hop neighbor
+                if (mBTToBNAddrMap.containsKey(address)) {
+                    advPayload.setOneHopNeighbor(new String(mBTToBNAddrMap.get(address)));
+                }
+
+                //set pull callback which is just a characteristic read
+                advPayload.setRetriever(new MessageRetriever() {
+                    @Override
+                    public byte [] retrieve(byte [] id) {
+                        BluetoothGattCharacteristic characteristic = bluetoothGatt
+                                .getService(BLUENET_SERVICE_UUID)
+                                .getCharacteristic(PULL_MESSAGE_CHAR_UUID);
+                        bluetoothGatt.readCharacteristic(characteristic);
+                        return null;
+                    }
+                });
+
+                mReadCB.read(advPayload);
             }
 
         }
     };
 
-    public boolean connect(final BluetoothDevice mDevice){
-        if (mBluetoothAdapter == null) {
-            Log.w(ERR_TAG, "BluetoothAdapter not initialized.");
-            return false;
-        }
 
-        // Previously connected device.  Try to reconnect.
-        if (mBluetoothGatt != null) {
-            Log.d(INFO_TAG, "Trying to use an existing mBluetoothGatt for connection.");
-            if (mBluetoothGatt.connect()) {
-                mConnectionState = STATE_CONNECTING;
-                return true;
-            } else {
-                return false;
-            }
-        }
-//
-//        final BluetoothDevice device = mBluetoothAdapter.getRemoteDevice().getAddress();
-//        if (device == null) {
-//            Log.w(TAG, "Device not found.  Unable to connect.");
-//            return false;
-//        }
-
-            //BluetoothDevice cdevice = mBluetoothAdapter.getRemoteDevice(mDevice.getAddress());
-            mBluetoothGatt = mDevice.connectGatt(context, false, mGattCallback);
-            return true;
-
-    }
     /**** **** End of Gatt Client **** ****/
-
-    @Override
-    public int read(String src, byte[] message) {
-        return 0;
-    }
-
-    @Override
-    public int read(AdvertisementPayload advPayload) {
-        return 0;
-    }
 
     @Override
     public String ask(String question) {
